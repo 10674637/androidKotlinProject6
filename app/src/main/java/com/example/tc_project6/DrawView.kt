523d35e693ca1859f5a8a31a322c23927337391f
Derek Carr <dcarr@ancestry.com>
2020-11-23 19:33:17 -0600

    Have movement and background paning

--- app/src/main/java/com/example/tc_project6/DrawView.kt
+++ app/src/main/java/com/example/tc_project6/DrawView.kt
@@ -13,8 +13,8 @@ import android.view.View
 import java.security.KeyStore
 
 class DrawView(context: Context, attrs: AttributeSet? = null): View(context, attrs) {
-    var nextX = 50F
-    var nextY = 50F
+    var nextX = 0F
+    var nextY = 0F
     var bitmap: Bitmap
     val out: Bitmap
     val water: Bitmap
@@ -22,10 +22,13 @@ class DrawView(context: Context, attrs: AttributeSet? = null): View(context, att
     val forest: Bitmap
     val plain: Bitmap
     val person: Bitmap
-    var personX: Int
-    var personY: Int
 
+    val blockSize = 155;
 
+    var xOffset = 0
+    var yOffset = 0
+
+    //12 X 12
     val arrayWorldMap = arrayOf(
         arrayOf("M","M","M","M","M",".",".",".","~","~","~","~"),
         arrayOf("M","M","M","M",".",".",".",".",".","~","~","~"),
@@ -73,20 +76,59 @@ class DrawView(context: Context, attrs: AttributeSet? = null): View(context, att
             R.drawable.person,
             null
         )
-        personX = 0
-        personY = 0
         bitmap = out
     }
 
     override fun onDraw(canvas: Canvas){
-        //check to see if bitmaps have been loaded
-//        canvas.drawBitmap(bitmap, nextX, nextY, null)
+        //protect edge fliy off
+//        if (nextX > (blockSize * 11 - xOffset)) {
+//            nextX = (blockSize * xOffset).toFloat()
+//        }
+//        if (nextY > (blockSize * 11 - yOffset)) {
+//            nextY = (blockSize * 10).toFloat()
+//        }
+
+        //check nextX see where
+//        if (nextX == 0F && nextY == 0F) {
+//
+//        } else {
+            if (nextX > blockSize * 5) {
+                xOffset = blockSize * 6
+                nextX = 0F
+            } else if (nextX < blockSize){
+                //if already to the left
+                if (xOffset == 0) {
+
+                }
+                // if on the right
+                else {
+                    xOffset = 0;
+                    nextX = (blockSize * 6).toFloat()
+                }
+            }
+            if (nextY > blockSize * 8) {
+                yOffset = blockSize * 7
+                nextY = (blockSize * 2).toFloat()
+            } else if (nextY < blockSize) {
+                //if top already
+                if (yOffset == 0) {
+
+                }
+                // bring back to top
+                else {
+                    yOffset = 0
+                    nextY = (blockSize * 3).toFloat()
+                }
+            }
+//        }
+
 
         //make a two dimensional array of character that tell which image go to which square
+//        for (var i = 0; i < arrayWorldMap.size; i++)
         arrayWorldMap.forEachIndexed { i, a ->
             a.forEachIndexed { j, b ->
                 //print(j)
-                println("i=$i j=$j b=$b")
+//                println("i=$i j=$j b=$b")
 //                var rect1 = Rect(i, j, i+12, j+12)
                 //SWITCH which bitmap to use.
                 when(b) {
@@ -96,15 +138,16 @@ class DrawView(context: Context, attrs: AttributeSet? = null): View(context, att
                     "." -> bitmap = plain
                     else -> bitmap = out
                 }
-                var x = (i * 128).toFloat()
-                var y = (j * 128).toFloat()
+                var x = (i * blockSize).toFloat() - yOffset
+                var y = (j * blockSize).toFloat() - xOffset
 
                 canvas.drawBitmap(bitmap, y, x, null)
 
             }
         }
         //here is where I put the person dot
-        canvas.drawBitmap(person, nextY, nextX, null)
+        canvas.drawBitmap(person, nextX, nextY, null)
+        println("nextX=${nextX}, nextY=${nextY}")
 
     }
 
@@ -112,13 +155,19 @@ class DrawView(context: Context, attrs: AttributeSet? = null): View(context, att
 
         if(event.action == MotionEvent.ACTION_UP){
             Log.i("CS3680", "Touch event" + event.getX() + "," + event.getY())
-            nextX = event.getX()
-            nextY = event.getY()
+            nextX = event.getX() - (event.getX() % blockSize)
+            nextY = event.getY() - (event.getY() % blockSize)
             invalidate()    //indirectly call onDraw make the image show where you click
             return true
         }
         return true
         //return super.onTouchEvent(event)
+    }
+
+    fun checkEdge(position: Int) {
+        if (position > blockSize * 12)  {
+
+        }
     }
 
 }
